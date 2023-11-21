3ff3a35cece40acde175134a43d5ef863ec1e534
evil-coconut <richard.morgan2951@yandex.ru>
2023-11-21 18:12:17 +0700

    Add a list and linkend elements

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
-<script setup>
-
-</script>
-
 <template>
-
+    <v-sheet
+        max-width="500"
+        class="d-flex flex-column justify-center pt-5 ml-auto mr-auto">
+        <Input
+            @submit="sendRequest"
+            labelName="Подсказки по адресам"
+            buttonName="Поиск"
+        />
+        <v-list 
+            max-width="500"
+            lines="one"
+            class="d-flex flex-column mb-6">
+            <v-list-item 
+                v-for="it in items"
+                :title="it">
+            </v-list-item>
+        </v-list>
+        <div>{{ error }}</div>
+    </v-sheet>
 </template>
 
-<style scoped>
+<script setup>
+    import Input from './components/Input.vue'
+    import {error, items, sendRequest} from './js/Request.js'
+</script>
 
+<style>
 
 </style>
