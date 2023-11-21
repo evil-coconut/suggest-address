00fa7f807102be143e9d9c4c4b75ba9c77cb9495
evil-coconut <richard.morgan2951@yandex.ru>
2023-11-21 18:03:50 +0700

    Add Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-form
+        class="d-flex flex-row align-center"
+        @submit.prevent="$emit('submit', query)">
+        <v-text-field 
+            :label="labelName"
+            variant="outlined"
+            hide-details="auto"
+            v-model.trim="query">
+        </v-text-field>
+        <v-btn 
+            class="ml-2"
+            size="large"
+            variant="outlined"
+            type="submit">
+            {{ buttonName }}
+        </v-btn>
+    </v-form>
+</template>
+
+<script setup>
+    defineProps(['buttonName', 'labelName']);
+    defineEmits(['submit']);
+
+    let query = "";
+</script>
+
+<style>
+
+</style>
